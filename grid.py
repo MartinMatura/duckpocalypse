af1fe5d1e82753256cfb4bc372b45b4b69c48ee3
Finlay Murphy <2765063m@student.gla.ac.uk>
2025-10-25 20:28:50 +0100

    Refactor main into grid, and refine simulator to be compatible with the api

---
+++ grid.py
@@ -0,0 +1,29 @@
+import grid_square
+from ducks import Ducks
+
+def set_up_grid():
+    pois = {
+        "gym": [(0, 10), (8, 10), (9, 4), (10, 14), (14, 18), (16, 3), (17, 9)],
+        "shop": [(3, 2), (3, 10), (11, 17), (12, 6), (15, 9), (15, 15)],
+        "pub": [(3, 18), (6, 5), (7, 0), (10, 11), (14, 8), (14, 14), (18, 3)],
+        "library": [(5, 3), (8, 3), (8, 11), (8, 18), (10, 9), (13, 17), (14, 1)]
+    }
+    
+    grid = [[grid_square.GridSquare(None) for _ in range(20)] for _ in range(20)]
+
+    for poi, coords in pois.items():
+        for row, col in coords:
+            grid[row][col].POI = poi
+
+    return grid
+
+
+def print_grid(grid, occupied):
+    for i in range(len(grid)):
+        line = ""
+        for j in range(len(grid[0])):
+            if (i,j) in occupied:
+                line += "O "
+            else:
+                line += "_ "
+        print(line)
--- simulator.py
+++ simulator.py
@@ -1,31 +1,25 @@
+import grid
+
 class Simulator:
-    def __init__(self, ducks, grid):
+    def __init__(self, ducks, x, y):
         self.ducks = ducks
-        self.grid = grid
-        self.x_size = len(grid[0])
-        self.y_size = len(grid)
-        self.occupied = set()
+        self.grid = grid.set_up_grid()
+        self.x_size = len(self.grid[0])
+        self.y_size = len(self.grid)
+        self.occupied = set((x,y))
         self.neighbours = set()
 
-        self.started = False
-
-    #Starting is in the form [i,j]
-    def start_simulation(self, starting):
-        self.started = True
-        self.occupied.add(starting)
-        self.add_new_neighbours(starting)
+        self.add_new_neighbours((x,y))
     
     def simulate_step(self):
-        for _ in range(self.ducks.intelligence):
+        for _ in range(int(self.ducks.intelligence / 5)):
             target = self.ducks.choose_square(self.grid, self.neighbours)
             if self.grid_get(target).attack(self.ducks):
                 self.occupied.add(target)
                 self.neighbours.remove(target)
                 self.add_new_neighbours(target)
 
-        # print(self.occupied)
         self.ducks.reproduce()
-        # print(self.ducks.number)
 
 
     def add_new_neighbours(self, point):
