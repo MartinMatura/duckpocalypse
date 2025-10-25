84c0fe3e12c15f4deb54e62139d25248e0392314
Phoebe-Hope <phoebe.hope345@gmail.com>
2025-10-25 17:54:19 +0100

    Set up front end and a header

---
+++ frontend/src/DuckSimulator.jsx
@@ -0,0 +1,21 @@
+import { useState } from "react"
+import reactLogo from "./assets/react.svg"
+import viteLogo from "/vite.svg"
+import "./DuckSimulator.css"
+import React from "react";
+import Header from "./Header.jsx";
+
+
+function App() {
+
+  return (
+    <>
+      <Header /> 
+      <main style={{ padding: "2rem" }}>
+        <p>What the duck??</p>
+      </main>
+    </>
+  );
+}
+
+export default App;
