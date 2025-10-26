6a6de26ae2ce07d7b45c27161088e96c0764db0d
Phoebe-Hope <phoebe.hope345@gmail.com>
2025-10-26 01:28:55 +0000

    Deleted old versions of files and moved updated backend files into backed folder

--- backend/simulator.py
+++ backend/simulator.py
@@ -2,6 +2,12 @@ import grid
 import random
 
 class Simulator:
+    INTELLIGENCE_PER_ATTACK = 5
+    MINIMUM_FOR_DUCK_LOSS = 15
+    NEIGHBOUR_COUNT_MODIFIER = 16
+    NEIGHBOUR_FREQUENCY = 3
+    FOOD_SUPPLY_MODIFIER = 75
+
     def __init__(self, ducks, x, y):
         self.ducks = ducks
         self.grid = grid.set_up_grid()
@@ -10,13 +16,18 @@ class Simulator:
         self.occupied = set()
         self.occupied.add((x,y))
         self.neighbours = set()
+        is_done = 0
 
         self.add_new_neighbours((x,y))
     
     def simulate_step(self):
         self.lose_ducks()
         #Choose new targets
-        for _ in range(int(self.ducks.intelligence / 5)):
+
+        for _ in range(int(self.ducks.intelligence / Simulator.INTELLIGENCE_PER_ATTACK)):
+            if len(self.occupied) == 400:
+                self.is_done = 1 
+                return  self.status_api_formatter()
             target = self.ducks.choose_square(self.grid, self.neighbours, self.occupied)
             if self.grid_get(target).attack(self.ducks):
                 self.occupied.add(target)
@@ -24,24 +35,25 @@ class Simulator:
                 self.add_new_neighbours(target)
 
         self.ducks.reproduce()
-        # print(list(self.occupied))
-        self.print_duck_stats()
+
         return self.status_api_formatter()
 
     def print_duck_stats(self):
         print("number: ",self.ducks.number, "   strength: ",self.ducks.strength,
               " happiness:",self.ducks.happiness, " food_supply: ", self.ducks.food_supply, " intelligence: ", self.ducks.intelligence)
 
+#Formats duck state and list of occupied cells to send as JSON to front end
     def status_api_formatter(self):
         return {"number":self.ducks.number, "happiness":self.ducks.happiness,
                    "food_supply":self.ducks.food_supply, "intelligence":self.ducks.intelligence,
-                   "strength":self.ducks.strength, "occupied":list(self.occupied)}
+                   "strength":self.ducks.strength, "occupied":list(self.occupied), "is_done": self.is_done}
 
     def add_new_neighbours(self, point):
         for points in self.get_all_neighbours(point):
             if points not in self.neighbours and points not in self.occupied:
                 self.neighbours.add(points)
 
+    #Return all neighbours around a given point 
     def get_all_neighbours(self, point):
         neighbours = []
         i = point[0] - 1
@@ -58,7 +70,7 @@ class Simulator:
 
 
     def lose_ducks(self):
-        if len(self.occupied) > 15:
+        if len(self.occupied) > Simulator.MINIMUM_FOR_DUCK_LOSS:
             neighbour_freq = dict()
             #frequency of all neighbours
             for neighbour in self.neighbours:
@@ -67,12 +79,10 @@ class Simulator:
                         neighbour_freq[adjacent] = neighbour_freq.get(adjacent, 0) + 1
             
             for k in neighbour_freq.keys():
-                if neighbour_freq[k] >=3 and (random.random() * neighbour_freq[k] / 16) > (self.ducks.food_supply/50):
+                if neighbour_freq[k] >=Simulator.NEIGHBOUR_FREQUENCY and (random.random() * neighbour_freq[k] / Simulator.NEIGHBOUR_COUNT_MODIFIER) > (self.ducks.food_supply/Simulator.FOOD_SUPPLY_MODIFIER):
                     self.grid_get(k).deoccupy(self.ducks)
                     self.occupied.remove(k)
                     self.neighbours.add(k)
-
-
 
 
     def grid_get(self, points):
