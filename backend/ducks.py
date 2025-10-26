b0acebf0a0cdde9cdab654a83fc0324fd98550a5
Phoebe-Hope <phoebe.hope345@gmail.com>
2025-10-26 01:46:30 +0100

    Moved backed files into backed/ folder for better structure

---
+++ backend/ducks.py
@@ -0,0 +1,21 @@
+import random
+
+class Ducks:
+
+    def __init__(self, number, happiness, food_supply, intelligence, strength, strategy):
+        self.number = number
+        self.happiness = happiness
+        self.food_supply = food_supply
+        self.intelligence = intelligence # amount of squares that can be attacked
+        self.strength = strength # probability of capturing a square
+        self.strategy = strategy
+
+    # Increase in food and happiness -> reproduction, Increase in intelligence less
+    def reproduce(self):
+        growth = (self.happiness + self.food_supply - 1.5 * self.intelligence)
+        growth = max(-0.1, min(0.1, (growth + random.uniform(-0.05, 0.05)) / 100))
+        self.number = round(self.number * (1 + growth))
+
+    # Chosen strategy method
+    def choose_square(self, grid, neighbours, occupied):
+        return self.strategy(grid, neighbours, occupied)
---
+++ backend/grid.py
@@ -0,0 +1,34 @@
+import grid_square
+
+pois = {
+    "gym": [(0, 10), (8, 10), (9, 4), (10, 14), (14, 18), (16, 3), (17, 9)],
+    "shop": [(3, 2), (3, 10), (11, 17), (12, 6), (15, 9), (15, 15)],
+    "pub": [(3, 18), (6, 5), (7, 0), (10, 11), (14, 8), (14, 14), (18, 3)],
+    "library": [(5, 3), (8, 3), (8, 11), (8, 18), (10, 9), (13, 17), (14, 1)]
+}
+
+def set_up_grid():
+    grid = [[grid_square.GridSquare(None) for _ in range(20)] for _ in range(20)]
+
+    for poi, coords in pois.items():
+        for row, col in coords:
+            grid[row][col].POI = poi
+
+    return grid
+
+
+def print_grid(grid, occupied):
+    for i in range(len(grid)):
+        line = ""
+        for j in range(len(grid[0])):
+            if (i,j) in occupied:
+                if grid[i][j].POI:
+                    line += "X "
+                else:
+                    line += "O "
+            else:
+                if grid[i][j].POI:
+                    line += "+ "
+                else:
+                    line += "_ "
+        print(line)
